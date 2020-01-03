367def8029b697b79dc426a8947082f351d9fb16
Ishan Dassanayake <ishansd94@gmail.com>
2020-01-03 23:32:09 +0530

    Modify CI

--- release.sh
+++ release.sh
@@ -19,19 +19,16 @@ TAG=`git describe --tags --abbrev=0 | awk -F. '{$NF+=1; OFS="."; print $0}'`
 
 echo "Next version is: $TAG"
 
-git tag ${TAG}
-git push origin --tags
+perl -i -pe "s/kubernetes-external-secrets Image tag \| \`[a-zA-Z0-9\.]*/kubernetes-external-secrets Image tag \| \`$TAG/" charts/kubernetes-external-secrets/README.md
+perl -i -pe "s/tag: [a-zA-Z0-9\.]*/tag: $TAG/" charts/kubernetes-external-secrets/values.yaml
+perl -i -pe "s/appVersion: [a-zA-Z0-9\.]*/appVersion: $TAG/" charts/kubernetes-external-secrets/Chart.yaml
+perl -i -pe "s/version: [a-zA-Z0-9\.]*/version: $TAG/" charts/kubernetes-external-secrets/Chart.yaml
+(cd charts/kubernetes-external-secrets && helm package . && helm repo index --merge ../../docs/index.yaml ./ && mv *.tgz ../../docs && mv index.yaml ../../docs)
 
-#perl -i -pe "s/kubernetes-external-secrets Image tag \| \`[a-zA-Z0-9\.]*/kubernetes-external-secrets Image tag \| \`$TAG/" charts/kubernetes-external-secrets/README.md
-#perl -i -pe "s/tag: [a-zA-Z0-9\.]*/tag: $TAG/" charts/kubernetes-external-secrets/values.yaml
-#perl -i -pe "s/appVersion: [a-zA-Z0-9\.]*/appVersion: $TAG/" charts/kubernetes-external-secrets/Chart.yaml
-#perl -i -pe "s/version: [a-zA-Z0-9\.]*/version: $TAG/" charts/kubernetes-external-secrets/Chart.yaml
-#(cd charts/kubernetes-external-secrets && helm package . && helm repo index --merge ../../docs/index.yaml ./ && mv *.tgz ../../docs && mv index.yaml ../../docs)
-#
-#docker build -t pearsontechnology/kubernetes-external-secrets:$SHA .
-#docker tag pearsontechnology/kubernetes-external-secrets:$SHA godaddy/kubernetes-external-secrets:$TAG
-#docker tag pearsontechnology/kubernetes-external-secrets:$SHA godaddy/kubernetes-external-secrets:latest
-#
-#git add --all && git commit -m "chore(release): pearsontechnology/kubernetes-external-secrets:$TAG"
-#git push --follow-tags origin master && docker push godaddy/kubernetes-external-secrets:$TAG && docker push godaddy/kubernetes-external-secrets:latest
-#
+docker build -t pearsontechnology/kubernetes-external-secrets:$SHA .
+docker tag pearsontechnology/kubernetes-external-secrets:$SHA godaddy/kubernetes-external-secrets:$TAG
+docker tag pearsontechnology/kubernetes-external-secrets:$SHA godaddy/kubernetes-external-secrets:latest
+
+git add --all && git commit -m "chore(release): pearsontechnology/kubernetes-external-secrets:$TAG"
+git push --follow-tags origin master && docker push godaddy/kubernetes-external-secrets:$TAG && docker push godaddy/kubernetes-external-secrets:latest
+
