178968922fd83d30aee47e18688ac6a0bc5dec46
Ishan Dassanayake <ishansd94@gmail.com>
2020-01-07 10:42:43 +0530

    chore(release): pearsontechnology/kubernetes-external-secrets:2.2.2 [ci skip]

--- release.sh
+++ release.sh
@@ -12,9 +12,9 @@ perl -i -pe "s/kubernetes-external-secrets Image tag \| \`[a-zA-Z0-9\.]*/kuberne
 perl -i -pe "s/tag: [a-zA-Z0-9\.]*/tag: $TAG/" charts/kubernetes-external-secrets/values.yaml
 perl -i -pe "s/appVersion: [a-zA-Z0-9\.]*/appVersion: $TAG/" charts/kubernetes-external-secrets/Chart.yaml
 perl -i -pe "s/version: [a-zA-Z0-9\.]*/version: $TAG/" charts/kubernetes-external-secrets/Chart.yaml
-(cd charts/kubernetes-external-secrets && helm init --client-only && helm package . && helm repo index --merge ../../docs/index.yaml ./ && mv *.tgz ../../docs && mv index.yaml ../../docs)
+(cd charts/kubernetes-external-secrets && helm package . && helm repo index --merge ../../docs/index.yaml ./ && mv *.tgz ../../docs && mv index.yaml ../../docs)
 
-docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
+#docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
 
 docker build -t pearsontechnology/kubernetes-external-secrets:$SHA .
 docker tag pearsontechnology/kubernetes-external-secrets:$SHA pearsontechnology/kubernetes-external-secrets:$TAG
